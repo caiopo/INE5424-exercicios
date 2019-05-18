2b58b5481b9a1a3eae8f8ad4a6c3a259a9ac8e0d
caiopo <caiopoliveira@gmail.com>
2019-05-18 19:12:03 -0300

    Improve scheduler_test

--- scheduling/app/scheduler_test.cc
+++ scheduling/app/scheduler_test.cc
@@ -9,14 +9,23 @@ OStream cout;
 Thread* thread_io;
 Thread* thread_cpu;
 
+int slow_fibonacci(int i) {
+    if (i == 0) return 0;
+    if (i == 1) return 1;
+
+    return slow_fibonacci(i - 1) + slow_fibonacci(i - 2);
+}
+
 // IO bound thread will not lose priority,
 // as it never uses its entire quantum
 int io_bound() {
-    cout << "IO bound thread at " << thread_io << endl;
+    cout << "IO bound thread at " << thread_io << " with priority = " << thread_io->priority() << endl;
 
-    while (1) {
+    for (int i = 0; i < 5; i++) {
         Alarm::delay(100000);
     }
+
+    cout << "IO bound thread finished " << endl;
 
     return 0;
 }
@@ -24,9 +33,11 @@ int io_bound() {
 // CPU bound thread will lose priority,
 // as it always uses its entire quantum
 int cpu_bound() {
-    cout << "CPU bound thread at " << thread_cpu << endl;
+    cout << "CPU bound thread at " << thread_cpu << " with priority = " << thread_cpu->priority() << endl;
 
-    while (1);
+    slow_fibonacci(35);
+
+    cout << "CPU bound thread finished " << endl;
 
     return 0;
 }
@@ -37,6 +48,9 @@ int main() {
 
     thread_io->join();
     thread_cpu->join();
+
+    cout << "CPU bound thread end priority: " << thread_cpu->priority() << endl;
+    cout << "IO bound thread end priority: " << thread_io->priority() << endl;
 
     delete thread_io;
     delete thread_cpu;
